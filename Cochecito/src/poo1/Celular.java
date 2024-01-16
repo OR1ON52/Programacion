f6bd24852728a14dd21bc056647995598dd2326d
sofia <adriansutil2004@gmail.com>
2024-01-16 08:51:59 +0100

    dime? dime? esto es lo q tu querias

---
+++ Cochecito/src/poo1/Celular.java
@@ -0,0 +1,15 @@
+package poo1;
+
+public class Celular {
+
+	String modelo;
+	int almanecimiento;
+	int camaraMP;
+	
+public Celular() {
+	
+	modelo="Generico";
+	almanecimiento=64;
+	camaraMP=12;
+}
+}
---
+++ Cochecito/src/poo1/uso_celular.java
@@ -0,0 +1,13 @@
+package poo1;
+
+public class uso_celular {
+
+	public static void main(String[] args) {
+		
+		Celular Nokia = new Celular();
+		System.out.println("Modelo: "+Nokia.modelo);
+		System.out.println("Almanecimiento: "+Nokia.almanecimiento+" GB");
+
+	}
+
+}
